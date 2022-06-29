87fe5fecd4a1c8a375fe203c71e9b45c2d21a8bb
ColetteJParker91 <103423447+ColetteJParker91@users.noreply.github.com>
2022-06-29 20:34:46 +0200

    Changed token password

--- passport.js
+++ passport.js
@@ -30,7 +30,7 @@ passport.use(new LocalStrategy({
 
 passport.use(new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey: 'your_jwt_secret'
+  secretOrKey: 'f1987hdg56'
 }, (jwtPayload, callback) => {
   return Users.findById(jwtPayload._id)
     .then((user) => {
