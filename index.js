4ae0a59e8b29567ca550b8f996c85bfdbad79f05
ColetteJParker91 <103423447+ColetteJParker91@users.noreply.github.com>
2022-07-01 00:23:46 +0200

    Set up for heroku
    Added heroku

--- index.js
+++ index.js
@@ -316,6 +316,7 @@ app.use((err, req, res, next) => {
   res.status(500).send('Oops, something broke!');
 });
 
-app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+ console.log('Listening on Port ' + port);
 });
