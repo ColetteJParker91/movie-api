94170b3aec0c028b3bb412cbb383eb97a235a5c6
ColetteJParker91 <103423447+ColetteJParker91@users.noreply.github.com>
2022-07-02 00:56:02 +0200

    Update mongoose connect

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/test', { useNewURLParser: true, useUnifiedTopology: true});
+//mongoose.connect('mongodb://localhost:27017/test', { useNewURLParser: true, useUnifiedTopology: true});
+mongoose.connect (process.env.CONNECTION_URI, {useNewURLParser: true,useUnifiedTopology: true});
 
 const express = require('express');
 const app = express();
